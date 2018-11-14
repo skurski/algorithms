096ce8608e3f938f3238ac3efa4a8e2ec0424776
Peter Skurski <skurski@users.noreply.github.com>
2018-11-14 15:25:42 +0100

    Create CustomTree.java

---
+++ src/datastructure/tree/CustomTree.java
@@ -0,0 +1,60 @@
+package datastructure.tree;
+
+import java.util.*;
+
+/**
+ * Tree data structure implementation
+ */
+public class CustomTree {
+
+    public static void main(String[] args) {
+		Node a = new Node(1, null, null);
+		Node b = new Node(2, a, null);
+		Node c = new Node(3, b, null);
+		
+		Node x = new Node(8, null, null);
+		Node y = new Node(9, x, null);
+		
+		Node root = new Node(4, c, y);
+		
+		System.out.println("In order traverse: -------------------");
+		traverseInOrder(root);
+		System.out.println("Pre order traverse: -------------------");
+		traversePreOrder(root);
+		System.out.println("Post order traverse: -------------------");
+		traversePostOrder(root);
+    }
+	
+	public static void traverseInOrder(Node root) {
+		if (root == null) return;
+		traverseInOrder(root.left);
+		System.out.println("Tree value: " + root.value);
+		traverseInOrder(root.right);
+	}
+	
+	public static void traversePreOrder(Node root) {
+		if (root == null) return;
+		System.out.println("Tree value: " + root.value);
+		traverseInOrder(root.left);
+		traverseInOrder(root.right);
+	}
+	
+	public static void traversePostOrder(Node root) {
+		if (root == null) return;
+		traverseInOrder(root.left);
+		traverseInOrder(root.right);
+		System.out.println("Tree value: " + root.value);
+	}
+	
+	private static class Node {
+		Node left;
+		Node right;
+		int value;
+		
+		Node (int value, Node left, Node right) {
+			this.left = left;
+			this.right = right;
+			this.value = value;
+		}
+	}
+}
